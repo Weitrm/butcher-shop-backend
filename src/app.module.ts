4618a528ed47cd2a28997a5eb3672da94ff13c4c
rodri 2 <rodrigomesaw@gmail.com>
2026-03-18 11:35:45 -0300

    migration_fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
@@ -32,6 +33,8 @@ const isProd = process.env.STAGE === 'prod';
       password: process.env.DB_PASSWORD,      
       autoLoadEntities: true,
       synchronize: !isProd,
+      migrations: [join(__dirname, 'migrations', '*{.ts,.js}')],
+      migrationsRun: isProd,
     }),
 
     ProductsModule,
